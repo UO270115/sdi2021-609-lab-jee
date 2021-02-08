6e8c0778a183615d97c4823cb7769b50ed7bbfd9
uo270115 <uo270115@uniovi.es>
2021-02-08 20:53:00 +0100

    SDI-2021-609-2.2-Trabajando con Controladores.

---
+++ sdi2021-609-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -0,0 +1,35 @@
+package com.uniovi.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.uniovi.entities.Mark;
+
+@RestController
+public class MarksControllers {
+
+	@RequestMapping("/mark/list")
+	public String getList() {
+		return "Getting List";
+	}
+
+//	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
+//	public String setMark(@RequestParam String description, @RequestParam String score) {
+//		return "Added: " + description + " with score: " + score;
+//	}
+
+	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
+	public String setMark(@ModelAttribute Mark mark) {
+		return "added: " + mark.getDescription() + " with score : " + mark.getScore() + " id: " + mark.getId();
+	}
+
+//	@RequestMapping("/mark/details")
+//	public String getDetail(@RequestParam Long id) {
+//		return " Getting Detail: " + id;
+//	}
+
+	@RequestMapping("/mark/details/{id}")
+	public String getDetail(@PathVariable Long id) {
+		return " Getting Detail: " + id;
+	}
+
+}
