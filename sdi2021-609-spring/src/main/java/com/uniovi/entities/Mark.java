1a0220d802d803eb83378a11d02855e0818a1a7c
uo270115 <uo270115@uniovi.es>
2021-02-16 19:05:59 +0100

    SDI-2021-609-3.3-Acceso a datos con entidades relacionadas.

--- sdi2021-609-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi2021-609-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -11,6 +11,10 @@ public class Mark {
 	private String description;
 	private Double score;
 
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
 	public Mark() {
 	}
 
@@ -19,6 +23,21 @@ public class Mark {
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 	public Long getId() {
