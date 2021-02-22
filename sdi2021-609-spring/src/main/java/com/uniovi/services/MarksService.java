4482e635a8ba77986b12f68b2a681e49b861937f
uo270115 <uo270115@uniovi.es>
2021-02-22 22:57:28 +0100

    SDI-2021-609-4.3-Control de acceso por roles.

--- sdi2021-609-spring/src/main/java/com/uniovi/services/MarksService.java
+++ sdi2021-609-spring/src/main/java/com/uniovi/services/MarksService.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.Mark;
@@ -47,23 +49,12 @@ public class MarksService {
 		marksRepository.deleteById(id);
 	}
 
-	/*
-	 * private List<Mark> marksList = new LinkedList<Mark>();
-	 * 
-	 * @PostConstruct public void init() { marksList.add(new Mark(1L, "Ejercicio 1",
-	 * 10.0)); marksList.add(new Mark(2L, "Ejercicio 2", 9.0)); }
-	 * 
-	 * public List<Mark> getMarks() { return marksList; }
-	 * 
-	 * public Mark getMark(Long id) { return marksList.stream().filter(mark ->
-	 * mark.getId().equals(id)).findFirst().get(); }
-	 * 
-	 * public void addMark(Mark mark) { // Si en Id es null le asignamos el ultimo +
-	 * 1 de la lista if (mark.getId() == null) {
-	 * mark.setId(marksList.get(marksList.size() - 1).getId() + 1); }
-	 * marksList.add(mark); }
-	 * 
-	 * public void deleteMark(Long id) { marksList.removeIf(mark ->
-	 * mark.getId().equals(id)); }
-	 */
+	public void setMarkResend(boolean revised, Long id) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String dni = auth.getName();
+		Mark mark = marksRepository.findById(id).get();
+		if (mark.getUser().getDni().equals(dni)) {
+			marksRepository.updateResend(revised, id);
+		}
+	}
 }
